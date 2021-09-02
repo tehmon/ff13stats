8e38dafdc233cdac78a12e3d68f6a6ddfa30ac6a
tehmon <79539299+tehmon@users.noreply.github.com>
2021-09-02 22:19:31 +0100

    Update create.py
    optimisation

--- create.py
+++ create.py
@@ -1,40 +1,12 @@
+characters = ["sazh", "lightning", "snow", "hope", "vanille", "fang"]
+stats = ["hp", "str", "mag"]
+
 def main():
-    with open("../files/sazh_hp.txt", "w+") as sazh_hp:
-        pass
-    with open("../files/sazh_str.txt", "w+") as sazh_str:
-        pass
-    with open("../files/sazh_mag.txt", "w+") as sazh_mag:
-        pass
-    with open("../files/lightning_hp.txt", "w+") as lightning_hp:
-        pass
-    with open("../files/lightning_str.txt", "w+") as lightning_str:
-        pass
-    with open("../files/lightning_mag.txt", "w+") as lightning_mag:
-        pass
-    with open("../files/snow_hp.txt", "w+") as snow_hp:
-        pass
-    with open("../files/snow_str.txt", "w+") as snow_str:
-        pass
-    with open("../files/snow_mag.txt", "w+") as snow_mag:
-        pass
-    with open("../files/hope_hp.txt", "w+") as hope_hp:
-        pass
-    with open("../files/hope_str.txt", "w+") as hope_str:
-        pass
-    with open("../files/hope_mag.txt", "w+") as hope_mag:
-        pass
-    with open("../files/vanille_hp.txt", "w+") as vanille_hp:
-        pass
-    with open("../files/vanille_str.txt", "w+") as vanille_str:
-        pass
-    with open("../files/vanille_mag.txt", "w+") as vanille_mag:
-        pass
-    with open("../files/fang_hp.txt", "w+") as fang_hp:
-        pass
-    with open("../files/fang_str.txt", "w+") as fang_str:
-        pass
-    with open("../files/fang_mag.txt", "w+") as fang_mag:
-        pass
+    for c in characters:
+        for s in stats:
+            file_name = "../files/" + c + "_" + s + ".txt"
+            with open(file_name, "w+") as f:
+                pass
 
 if __name__ == "__main__":
     main()
