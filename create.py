0930dba43d8d8988d4bf4a4f84b8265c9ace8f62
tehmon <79539299+tehmon@users.noreply.github.com>
2021-09-02 22:04:29 +0100

    Create create.py

---
+++ create.py
@@ -0,0 +1,40 @@
+def main():
+    with open("../files/sazh_hp.txt", "w+") as sazh_hp:
+        sazh_hp.write("0")
+    with open("../files/sazh_str.txt", "w+") as sazh_str:
+        sazh_str.write("0")
+    with open("../files/sazh_mag.txt", "w+") as sazh_mag:
+        sazh_mag.write("0")
+    with open("../files/lightning_hp.txt", "w+") as lightning_hp:
+        lightning_hp.write("0")
+    with open("../files/lightning_str.txt", "w+") as lightning_str:
+        lightning_str.write("0")
+    with open("../files/lightning_mag.txt", "w+") as lightning_mag:
+        lightning_mag.write("0")
+    with open("../files/snow_hp.txt", "w+") as snow_hp:
+        snow_hp.write("0")
+    with open("../files/snow_str.txt", "w+") as snow_str:
+        snow_str.write("0")
+    with open("../files/snow_mag.txt", "w+") as snow_mag:
+        snow_mag.write("0")
+    with open("../files/hope_hp.txt", "w+") as hope_hp:
+        hope_hp.write("0")
+    with open("../files/hope_str.txt", "w+") as hope_str:
+        hope_str.write("0")
+    with open("../files/hope_mag.txt", "w+") as hope_mag:
+        hope_mag.write("0")
+    with open("../files/vanille_hp.txt", "w+") as vanille_hp:
+        vanille_hp.write("0")
+    with open("../files/vanille_str.txt", "w+") as vanille_str:
+        vanille_str.write("0")
+    with open("../files/vanille_mag.txt", "w+") as vanille_mag:
+        vanille_mag.write("0")
+    with open("../files/fang_hp.txt", "w+") as fang_hp:
+        fang_hp.write("0")
+    with open("../files/fang_str.txt", "w+") as fang_str:
+        fang_str.write("0")
+    with open("../files/fang_mag.txt", "w+") as fang_mag:
+        fang_mag.write("0")
+
+if __name__ == "__main__":
+    main()
