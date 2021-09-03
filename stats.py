8865f425da4c5fd3ea9003986f40b3027b34ac06
tehmon <79539299+tehmon@users.noreply.github.com>
2021-09-03 14:44:23 +0100

    add create directory function
    Added the  function for creating directories and files into the main script to reduce number of files

--- stats.py
+++ stats.py
@@ -1,3 +1,4 @@
+import os
 from os import listdir
 import pymem
 
@@ -12,9 +13,25 @@ char_base = {
                 "lightning": pm.read_int(b_address+0x2402D90)
             }
 offsets = {"hp": 0xDB8 , "str": 0xDC8 , "mag": 0xDCC}
-files_in_dir = [f for f in listdir("../files")]
+files_in_dir = []
+
+def create():
+    if not os.path.exists("files"):
+        os.mkdir("files")
+    if not os.listdir("files"):
+        for c in char_base:
+            for s in offsets:
+                file_name = "files/" + c + "_" + s + ".txt"
+                with open(file_name, "w+") as f:
+                    pass
 
 def main():
+    if not os.path.exists("files"):
+        create()
+    elif not listdir("files"):
+        create()
+
+    files_in_dir = [f for f in listdir("files")]
     while True:
         for file in files_in_dir:
             file_path = f"../files/{file}"
