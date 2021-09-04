973d6f89fb91d2387479b27bb5f872584d954126
tehmon <79539299+tehmon@users.noreply.github.com>
2021-09-04 22:37:47 +0100

    Update stats.py
    added inventory stats

--- stats.py
+++ stats.py
@@ -1,27 +1,31 @@
 import os
+import time
 from os import listdir
 import pymem
 
 pm = pymem.Pymem("ffxiiiimg.exe")
 b_address = pm.base_address
 try:
+    inv_base = pm.read_int(b_address+0x28BF440)
     char_base = {
                 "sazh": pm.read_int(b_address+0x28BF608), 
                 "snow": pm.read_int(b_address+0x28BF638), 
                 "fang": pm.read_int(b_address+0x28BF590), 
-                "hope": pm.read_int(b_address+0x28BF5C0) ,
-                "vanille": pm.read_int(b_address+0x28BF650) , 
+                "hope": pm.read_int(b_address+0x28BF5C0),
+                "vanille": pm.read_int(b_address+0x28BF650), 
                 "lightning": pm.read_int(b_address+0x28BF5F0)
                 }
 except:
+    inv_base = pm.read_int(b_address+0x2402BE0)
     char_base = {
                 "sazh": pm.read_int(b_address+0x2402DA8), 
                 "snow": pm.read_int(b_address+0x2402DD8), 
                 "fang": pm.read_int(b_address+0x2402D30), 
-                "hope": pm.read_int(b_address+0x2402D60) ,
-                "vanille": pm.read_int(b_address+0x2402DF0) , 
+                "hope": pm.read_int(b_address+0x2402D60),
+                "vanille": pm.read_int(b_address+0x2402DF0), 
                 "lightning": pm.read_int(b_address+0x2402D90)
                 }
+inv_offsets = {"gil": 0x2578, "crystage": 0x258C}
 offsets = {"hp": 0xDB8 , "str": 0xDC8 , "mag": 0xDCC}
 files_in_dir = []
 
@@ -29,6 +33,10 @@ def create():
     if not os.path.exists("files"):
         os.mkdir("files")
     if not os.listdir("files"):
+        for inv in inv_offsets:
+            file_name = "files/" + inv + ".txt"
+            with open(file_name, "w+") as f:
+                pass
         for c in char_base:
             for s in offsets:
                 file_name = "files/" + c + "_" + s + ".txt"
@@ -43,15 +51,21 @@ def main():
 
     files_in_dir = [f for f in listdir("files")]
     while True:
+        
         for file in files_in_dir:
-            file_path = f"../files/{file}"
+            for inv in inv_offsets:
+                if inv in file:
+                    file_path = f"files/{file}"
+                    with open(file_path, "r+")as f: 
+                        f.write(str(pm.read_int(inv_base + inv_offsets[inv])))
+                        f.truncate()
             for c_key in char_base:
                 if c_key in file:
+                    file_path = f"files/{file}"
                     with open(file_path, "r+") as f:
                         for o_key in offsets:
                             if o_key in file:
                                 f.write(str(pm.read_int(char_base[c_key]+offsets[o_key])))
                                 f.truncate()
-
 if __name__ == "__main__":
     main()
