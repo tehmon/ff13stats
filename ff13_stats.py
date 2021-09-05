52bef51804f9f9dacfd7973c14c0f01f0994c7a6
tehmon <79539299+tehmon@users.noreply.github.com>
2021-09-05 03:05:39 +0100

    Update and rename stats.py to ff13_stats.py
    updated script name and directory names.

--- ff13_stats.py
+++ ff13_stats.py
@@ -30,38 +30,38 @@ offsets = {"hp": 0xDB8 , "str": 0xDC8 , "mag": 0xDCC}
 files_in_dir = []
 
 def create():
-    if not os.path.exists("files"):
-        os.mkdir("files")
-    if not os.listdir("files"):
+    if not os.path.exists("ff13_files"):
+        os.mkdir("ff13_files")
+    if not os.listdir("ff13_files"):
         for inv in inv_offsets:
-            file_name = "files/" + inv + ".txt"
+            file_name = "ff13_files/" + inv + ".txt"
             with open(file_name, "w+") as f:
                 pass
         for c in char_base:
             for s in offsets:
-                file_name = "files/" + c + "_" + s + ".txt"
+                file_name = "ff13_files/" + c + "_" + s + ".txt"
                 with open(file_name, "w+") as f:
                     pass
 
 def main():
-    if not os.path.exists("files"):
+    if not os.path.exists("ff13_files"):
         create()
-    elif not listdir("files"):
+    elif not listdir("ff13_files"):
         create()
 
-    files_in_dir = [f for f in listdir("files")]
+    files_in_dir = [f for f in listdir("ff13_files")]
     while True:
         
         for file in files_in_dir:
             for inv in inv_offsets:
                 if inv in file:
-                    file_path = f"files/{file}"
+                    file_path = f"ff13_files/{file}"
                     with open(file_path, "r+")as f: 
                         f.write(str(pm.read_int(inv_base + inv_offsets[inv])))
                         f.truncate()
             for c_key in char_base:
                 if c_key in file:
-                    file_path = f"files/{file}"
+                    file_path = f"ff13_files/{file}"
                     with open(file_path, "r+") as f:
                         for o_key in offsets:
                             if o_key in file:
